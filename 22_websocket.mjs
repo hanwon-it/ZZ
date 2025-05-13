499afcb472a25a3685adc356b38563d98f588bc6
hanwon <hanwon000128@gmail.com>
2025-05-13 10:57:13 +0900

    채팅_입장시_닉네임과_채널명_표시시

---
+++ 22_websocket.mjs
@@ -0,0 +1,34 @@
+/* 
+    웹소켓
+    웹서버는 웹 브라우저와 서버 사이애 지속적으로 연결을 유지하면서 실시간으로 데이터를 주고받을 수 있는 통신 방식
+    서버를 만들 떄 서버 소켓도 만들고 서버 소켓을 띄어놓게되면면 클라이언트가 아이피와 포트를 통해서 서버에 접속하게
+    될때 핸드쉐이크 기능을 이용해서 서로 연결된다. 그렇게 되면 클라이언트는 일반 소켓을 생성하게 되고 서버도 일반소켓을 만들어 일반 소켓끼리
+    통신하게 된다.
+*/
+import express from "express";
+import { createServer } from "http";
+import path from "path";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+// ES(.mjs)에서는 __dirname, __filename이 없다
+// import.meta.url: 현재 파일의 경로
+// fileUTLToPath: 실제 경로를 문자열로 변환
+// path.dirname: 디렉토리 이름만 추출
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("join", ({ nickname, channel }) => {
+    console.log("nickname: ", nickname, "channel: ", channel);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("서버 실행 중");
+});
